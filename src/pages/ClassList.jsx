b27ba595883a2ba09408166c7d7679fbe0c17b58
mman9our <hack_225@outlook.sa>
2023-09-15 20:41:13 +0300

    Initial

---
+++ src/pages/ClassList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ClassList = () => {
+  return (
+    <div className="box">
+      <h1></h1>
+      <h2>ClassList:</h2>
+    </div>
+  );
+};
+
+export default ClassList;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="box">{/* Links */}</div>;
+};
+
+export default Home;
