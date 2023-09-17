204a819d0c158edfd9fcf83d412a79796f2c602a
mman9our <hack_225@outlook.sa>
2023-09-17 22:29:08 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import MyForm from "./components/MyForm";
 import Devices from "./components/Devices";
-import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
+        <Devices />
       </header>
     </div>
   );
--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -1,92 +1,186 @@
+// MUI IMPORTS
+
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+
+import { v4 as uuidv4 } from "uuid";
 import React, { useState } from "react";
 
-let nextId = 5;
 function Devices() {
   const [deviceNameInputValue, setDeviceNameInputValue] = useState("");
+  const [updatedDeviceName, setUpdatedDeviceName] = useState("");
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
+
+  const [open, setOpen] = useState(false);
   const [devices, setDevices] = useState([
-    { id: 1, name: "iPone" },
-    { id: 2, name: "MacBook" },
-    { id: 3, name: "iPad" },
-    { id: 4, name: "Apple Watch" },
+    { id: uuidv4(), name: "iPhone" },
+    { id: uuidv4(), name: "MacBook" },
+    { id: uuidv4(), name: "iPad" },
+    { id: uuidv4(), name: "Apple Watch" },
   ]);
 
   const deviceList = devices.map((device) => {
     return (
-      <li key={device.id}>
-        {device.name}{" "}
-        <button
-          onClick={() => {
-            handleDeleteClick(device.id);
+      <ListItem key={device.id} >
+        <Box
+          sx={{
+            width: 800,
+            marginTop: "5px",
+            padding: "10px 10px 15px 10px",
+            borderRadius: "10px",
+            "&:hover": {
+              backgroundColor: "rgb(31, 38, 46)",
+            },
           }}
+          display="flex"
+          alignItems="center"
         >
-          Delete
-        </button>
-        <button
-          onClick={() => {
-            handleEditClick(device.id);
-          }}
-        >
-          Edit
-        </button>
-      </li>
+          <Box flexGrow={1}>{device.name}</Box>
+          <Box mr={2}>
+            <Button
+              onClick={() => {
+                handleDeleteClick(device.id);
+              }}
+              variant="outlined"
+              color="error"
+            >
+              Delete
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              onClick={() => {
+                handleOpenDialog(device.id);
+              }}
+              color="secondary"
+              variant="outlined"
+            >
+              Edit
+            </Button>
+          </Box>
+        </Box>
+      </ListItem>
     );
   });
 
-  function handleEditClick(id) {
-    const newDevices = devices.map((device) => {
-      if (device.id == id) {
-        let newDevice = { ...device, name: device.name + "0" };
-        return newDevice;
-      } else {
-        return device;
-      }
-    });
-    setDevices(newDevices);
-  }
-  function handleDeleteClick(id) {
-    // const newDevices = devices.filter((device) => {
-    //   if (device.id === id) return false;
+  const handleClose = () => {
+    setOpen(false);
+    setUpdatedDeviceName("");
+  };
 
-    //   return true;
-    // });
-
-    // setDevices(newDevices);
-
-    const newDevices = [...devices];
-
-    let index = 0;
-    let selectedIndex = 0;
-
-    for (let device of newDevices) {
-      if (device.id == id) {
-        selectedIndex = index;
-      }
-      index++;
+  const handleOpenDialog = (deviceId) => {
+    setSelectedDeviceId(deviceId); 
+    const deviceToUpdate = devices.find((device) => device.id === deviceId);
+    if (deviceToUpdate) {
+      setUpdatedDeviceName(deviceToUpdate.name);
     }
-    newDevices.splice(selectedIndex, 1);
+    setOpen(true);
+  };
+
+  const handleUpdateClick = () => {
+    const updatedDevices = devices.map((device) => {
+      if (device.id === selectedDeviceId) {
+        return { ...device, name: updatedDeviceName };
+      }
+      return device;
+    });
+    setDevices(updatedDevices);
+    handleClose();
+  };
+
+  function handleDeleteClick(id) {
+    const newDevices = devices.filter((device) => {
+      if (device.id === id) return false;
+      return true;
+    });
     setDevices(newDevices);
   }
 
   function handleAddClick() {
-    // const newDevices = [...devices];
-    // newDevices.push(deviceNameInputValue);
-    // setDevices(newDevices);
-
-    setDevices([...devices, { id: nextId, name: deviceNameInputValue }]);
-    nextId++;
+    const updatedDevices = [
+      ...devices,
+      { id: uuidv4(), name: deviceNameInputValue },
+    ];
+    setDevices(updatedDevices);
   }
 
   return (
-    <div>
-      <ul>{deviceList}</ul>
-      <input
-        value={deviceNameInputValue}
-        onChange={(event) => {
-          setDeviceNameInputValue(event.target.value);
+    <>
+      <Container
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
-      <button onClick={handleAddClick}>Add</button>
-    </div>
+      >
+        <List>{deviceList}</List>
+        <Stack
+          sx={{ width: 800, marginTop: "40px" }}
+          direction="row"
+          spacing={2}
+        >
+          <TextField
+            style={{ width: "80%" }}
+            value={deviceNameInputValue}
+            onChange={(event) => {
+              setDeviceNameInputValue(event.target.value);
+            }}
+            label="Device Name"
+            variant="filled"
+            InputLabelProps={{
+              style: { color: "#1976d2" },
+            }}
+            InputProps={{
+              style: { borderColor: "white", color: "#1976d2" },
+            }}
+            focused
+          />
+
+          <Button
+            style={{ width: "20%" }}
+            onClick={handleAddClick}
+            variant="outlined"
+          >
+            Add
+          </Button>
+        </Stack>
+      </Container>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Update Device Name</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To update the device name, please enter the new name below.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="deviceName"
+            label="New Device Name"
+            type="text"
+            fullWidth
+            value={updatedDeviceName}
+            onChange={(event) => setUpdatedDeviceName(event.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleUpdateClick}>Update</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
