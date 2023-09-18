fd106a841d3dc6b4cbe7440bcebce997302466ac
mman9our <hack_225@outlook.sa>
2023-09-18 11:14:00 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
-import Devices from "./components/Devices";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Devices />
-      </header>
+    <div className="todo-container">
+      <h1>
+        <span className="second-title">Todo List App</span>
+      </h1>
+      <form>
+        <input
+          className="add-task"
+          type="text"
+          placeholder="Add new task ..."
+        />
+        <button type="submit" className="add-button">
+          Add
+        </button>
+      </form>
+
+      <div className="todo">
+        <div className="todo-text">
+          <input className="checkbox" type="checkbox" id="isCompleted" />
+        </div>
+        <div>Learn useEffect Hook</div>
+
+        <div className="todo-actions">
+          <button className="submit-edits">Edit</button>
+          <button className="submit-edits">Delete</button>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
